eb3fa9c39b2fcaa23af748ef44219495389ef019
Bennie Rosas <bennie.rosas@blvd.co>
2024-05-30 00:13:54 -0500

    added google oauth button

---
+++ src/GoogleLoginButton.tsx
@@ -0,0 +1,29 @@
+import type { CredentialResponse } from '@react-oauth/google';
+
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
+import React from 'react';
+
+const GoogleLoginButton: React.FC = () => {
+    const handleLoginSuccess = (response: CredentialResponse) => {
+        const token = response.credential as string;
+        const decoded = jwtDecode(token);
+        console.log('Login Success:', decoded);
+        // Send the token to your backend for further processing
+    };
+
+    const handleLoginFailure = () => {
+        console.error('Login Failed');
+    };
+
+    return (
+        <div>
+            <GoogleLogin
+                onSuccess={handleLoginSuccess}
+                onError={handleLoginFailure}
+            />
+        </div>
+    );
+};
+
+export default GoogleLoginButton;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "./index.css"
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import App from "./App.tsx"
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 )
